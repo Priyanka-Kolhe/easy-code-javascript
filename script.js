5b49fb184df431346fd562bac38632582da252d7
priyanka-kolhe <pkolhe657@gmail.com>
2025-04-05 21:31:25 +0530

    Day4 Done

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Easy code is learning platform");
+
+console.warn(" complete and Add Assignments daily");
+
+console.error("Invalid id");
+
+console.info("if you want to visit Easy Code then you should go for given link (https://www.easycode.support/)");
