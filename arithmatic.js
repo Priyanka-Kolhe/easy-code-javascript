7468dc42f4fe854adf85f7d185be004f74388414
priyanka-kolhe <pkolhe657@gmail.com>
2025-04-18 10:16:30 +0530

    day8

---
+++ arithmatic.js
@@ -0,0 +1,26 @@
+// Arithmatic operators 
+
+console.log("Addition");
+let a = 5;
+let b = 4;
+console.log(a + b);
+
+console.log("Subtraction");
+let c = 5;
+let d = 4;
+console.log(c - d);
+
+console.log("Multipication");
+let p = 5;
+let r = 4;
+console.log(p * r);
+
+console.log("Division");
+let s = 5;
+let t = 4;
+console.log(s / t);
+
+console.log("Modulo");
+let x = 5;
+let y = 4;
+console.log(x % y);
