42a6d73e02424f2db70b632e91c1c66c577e7698
priyanka-kolhe <pkolhe657@gmail.com>
2025-04-19 18:57:15 +0530

    conditional statement

---
+++ day9/script.js
@@ -0,0 +1,11 @@
+// conditional statements
+
+let num = parseInt(prompt ("Enter a number : "));
+
+if(num % 2 == 0)
+{
+    console.log("Number is Even");
+}
+else{
+    console.log("Number is odd");
+}
