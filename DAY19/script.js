55a1762a5b10d220898b617129a08f9a2661e16d
priyanka-kolhe <pkolhe657@gmail.com>
2025-05-21 18:46:09 +0530

    day 19 done

---
+++ DAY19/script.js
@@ -0,0 +1,7 @@
+let box = document.getElementsByClassName("box");
+console.log(box);
+let p = document.getElementsByClassName("box1");
+box.innerHtml="good morning";
+
+let pa = document.getElementById("paragrap")
+console.log(pa)
